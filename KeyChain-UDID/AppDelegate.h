5909ab4cc261d63ed198c009495ef26f253fb50e
yongqianvip <yongqianvip@163.com>
2016-04-18 16:31:26 +0800

    base project

---
+++ KeyChain-UDID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChain-UDID
+//
+//  Created by YYQ on 16/4/18.
+//  Copyright © 2016年 YYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyChain-UDID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyChain-UDID
+//
+//  Created by YYQ on 16/4/18.
+//  Copyright © 2016年 YYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
